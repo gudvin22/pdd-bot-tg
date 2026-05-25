93246c4b77cb07c211a296c95dc4b3e5a99d4f3e
gudvin22 <64dro@rambler.ru>
2026-05-25 18:35:30 +0700

    git init

---
+++ src/main/java/com/pdd/pddbottg/PddBotTgApplication.java
@@ -0,0 +1,13 @@
+package com.pdd.pddbottg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PddBotTgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PddBotTgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdd/pddbottg/PddBotTgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdd.pddbottg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PddBotTgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
